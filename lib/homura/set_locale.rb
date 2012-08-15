91f0ce3555bd6447e124617de3dbb2b8c0cc50ab
ayaya <ayaya@ayaya.tw>
2012-08-16 00:08:31 +0800

    return the correct locale name

--- lib/homura/set_locale.rb
+++ lib/homura/set_locale.rb
@@ -10,22 +10,26 @@ module Homura
       @available_locales ||= I18n.available_locales.map(&:to_s)
     end
 
+    def locale_available?(locale)
+      available_locales.include?(locale)
+    end
+
     def set_locale
-      if params['locale'] && available_locales.include?(params['locale'])
+      if params['locale'] && locale_available?(params['locale'])
         cookies.permanent['locale'] = locale
         I18n.locale = locale and return
       end
 
-      if cookies['locale'] && available_locales.include?(locale)
+      if cookies['locale'] && locale_available?(locale)
         I18n.locale = locale and return
       end
 
       http_accept_language = request.headers['HTTP_ACCEPT_LANGUAGE'] || ''
       preferred_locales = http_accept_language.split(',').map { |l|
-        l.split(';').first
+        l.split(';').first.downcase.gsub(/-[a-z0-9]+$/i) { |x| x.upcase }
       }
       I18n.locale = preferred_locales.find { |l|
-        available_locales.include?(l)
+        locale_available?(l)
       }
     end
   end
